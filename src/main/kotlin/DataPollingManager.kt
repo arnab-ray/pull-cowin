577521d0c064f3d97ce0c24afeeb4b2500b6d7c3
arnab-ray <18110248+arnab-ray@users.noreply.github.com>
2021-05-04 07:23:06 +0530

    removed channel annotation

--- src/main/kotlin/DataPollingManager.kt
+++ src/main/kotlin/DataPollingManager.kt
@@ -104,6 +104,6 @@ object DataPollingManager {
 
     private fun sendSlackMessage(message: String, ageLimit: Int) {
         val slackIncomingWebHook = SlackIncomingWebhookConstructor.getIncomingWebHook(ageLimit)
-        slackIncomingWebHook.sendMessage(SlackMessage("<!channel> $message"))
+        slackIncomingWebHook.sendMessage(SlackMessage(message))
     }
 }
